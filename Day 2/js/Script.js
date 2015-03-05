1c488e4a79e3ab63f066f6dd170079a3a6b9111c
Rae Smith <rsmith1@fullsail.edu>
2015-03-05 14:54:01 -0500

    Day 2 Intro Files, Starting
    Created HTML and JS file for Day Number 2.

---
+++ Day 2/js/Script.js
@@ -0,0 +1,20 @@
+/*
+    Rae Smith
+    3-5-15
+    Section 00
+    Day 2, Intro
+ */
+
+//alert("Testing to see if connected.");
+
+/*
+Example of a multi-lined comment.
+Anything inside of here will not be seen by the computer.
+ */
+
+//  Single line comment.    ONLY this line will be ignored.
+
+//Alert box- pop up box that will alert the user with information.
+alert("Text that the user will see.");
+
+
