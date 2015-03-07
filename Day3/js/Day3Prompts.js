7220296333a85f84b711f25762339e3e3c39edca
Rae Smith <rsmith1@fullsail.edu>
2015-03-07 15:27:25 -0500

    Prompts
    Learned to prompt.

---
+++ Day3/js/Day3Prompts.js
@@ -0,0 +1,32 @@
+/*
+Rae Smith
+3-7-15
+Section 00
+Day 3 Prompts
+ */
+
+//alert("Just testing.");
+
+//Ask the user for input
+//prompt("Text that the user will see.")
+
+var userInput = prompt("Enter year of birth.");
+console.log(userInput);
+
+//Calculate the area of a rectangle
+//Length * Width
+
+//Prompt the User for the width and height
+//Explain to them whats going on.
+var height = prompt("Lets calculate the area of a rectangle. \nPlease enter the height of the rectangle.");
+
+var width = prompt("Please enter the width of the rectangle.");
+
+//Calculate the area of the rectangle.
+var area = width*height;
+
+//Tell the user the answer
+console.log("The area of the rectangle with a height of "+height+"  and a width of "+width+" is "+area+".");
+
+var results = "The results of the area are "+area;
+console.log(results):
