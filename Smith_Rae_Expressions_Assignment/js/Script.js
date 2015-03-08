f567940b8346a81ab9618267fc89a96831f93c6f
Rae Smith <rsmith1@fullsail.edu>
2015-03-08 15:43:38 -0400

    Expressions Assignment
    Wrote the code and made comments.

---
+++ Smith_Rae_Expressions_Assignment/js/Script.js
@@ -0,0 +1,47 @@
+/*
+Rae Smith
+3-8-15
+Section 00
+Expressions Assignment
+ */
+
+//alert ("Testing.");
+
+//Initializing an array.
+var numbers= ["8", "5", "Add"];
+
+//Inputting number.
+var userInput = prompt("Please enter a number.");
+numbers[0] = userInput;
+//console.log(numbers[0]);
+
+//Asking to add or subtract.
+userInput = prompt("Would you like to Add or Subtract?");
+numbers[1] = userInput;
+//console.log(numbers[1]);
+
+//Checking to see if it's addition or subtraction.
+if (numbers[1] == "add" || numbers[1] == "Add")
+{
+    //Inputting another number for addition.
+    userInput = prompt("Please enter a number to Add.");
+    numbers[2] = userInput;
+
+    //Creating a string for the answer.
+    var answer = Number(numbers[0]) + Number(numbers[2]);
+
+    //Showing the answer to the User.
+    console.log(numbers[0]+" + "+numbers[2]+" = "+answer);
+}
+else
+{
+   //Inputting another number for subtraction.
+    userInput = prompt("Please enter a number to Subtract.");
+    numbers[2] = userInput;
+
+    //Creating a string for the answer.
+    var answer = Number(numbers[0]) - Number(numbers[2]);
+
+    //Showing the answer to the User.
+    console.log(numbers[0]+" - "+numbers[2]+" = "+answer);
+}
