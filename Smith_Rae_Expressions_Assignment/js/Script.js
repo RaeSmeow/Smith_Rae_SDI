728d9b0f0a5693855b012823819936226f66dd9a
Rae Smith <rsmith1@fullsail.edu>
2015-03-08 15:47:02 -0400

    First Loop
    Added a loop.

--- Smith_Rae_Expressions_Assignment/js/Script.js
+++ Smith_Rae_Expressions_Assignment/js/Script.js
@@ -1,6 +1,6 @@
 /*
 Rae Smith
-3-8-15
+3-7-15
 Section 00
 Expressions Assignment
  */
@@ -20,28 +20,33 @@ userInput = prompt("Would you like to Add or Subtract?");
 numbers[1] = userInput;
 //console.log(numbers[1]);
 
+//If User puts something other then add or subtract.
+
+while(true) {
 //Checking to see if it's addition or subtraction.
-if (numbers[1] == "add" || numbers[1] == "Add")
-{
-    //Inputting another number for addition.
-    userInput = prompt("Please enter a number to Add.");
-    numbers[2] = userInput;
+    if (numbers[1] == "add" || numbers[1] == "Add") {
+        //Inputting another number for addition.
+        userInput = prompt("Please enter a number to Add.");
+        numbers[2] = userInput;
 
-    //Creating a string for the answer.
-    var answer = Number(numbers[0]) + Number(numbers[2]);
+        //Creating a string for the answer.
+        var answer = Number(numbers[0]) + Number(numbers[2]);
 
-    //Showing the answer to the User.
-    console.log(numbers[0]+" + "+numbers[2]+" = "+answer);
+        //Showing the answer to the User.
+        console.log(numbers[0] + " + " + numbers[2] + " = " + answer);
+        break;
+    }
+    if (numbers[1] == "Subtract" || numbers[1] == "subtract") {
+        //Inputting another number for subtraction.
+        userInput = prompt("Please enter a number to Subtract.");
+        numbers[2] = userInput;
+
+        //Creating a string for the answer.
+        var answer = Number(numbers[0]) - Number(numbers[2]);
+
+        //Showing the answer to the User.
+        console.log(numbers[0] + " - " + numbers[2] + " = " + answer);
+        break;
+    }
 }
-else
-{
-   //Inputting another number for subtraction.
-    userInput = prompt("Please enter a number to Subtract.");
-    numbers[2] = userInput;
-
-    //Creating a string for the answer.
-    var answer = Number(numbers[0]) - Number(numbers[2]);
-
-    //Showing the answer to the User.
-    console.log(numbers[0]+" - "+numbers[2]+" = "+answer);
-}
+//The End.
