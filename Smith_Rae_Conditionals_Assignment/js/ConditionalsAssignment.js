0ad639f93c5b70964d70b432875f779499756731
Rae Smith <rsmith1@fullsail.edu>
2015-03-12 21:28:09 -0400

    Comments.
    Added more comments.

--- Smith_Rae_Conditionals_Assignment/js/ConditionalsAssignment.js
+++ Smith_Rae_Conditionals_Assignment/js/ConditionalsAssignment.js
@@ -9,43 +9,58 @@ Scalable Data Infrastructures
 //alert("Testing.");
 
 //Initializing an array.
-var numbers= ["17", "42", "Add", "Subtract", "Multiply", "Divide"];
+var numbers= ["17", "42", "Add"];
 
-//Inputting the first number.
+//Loop for validation.
 while(true) {
-    var userInput = prompt("Please enter a number.")
+    //Prompting the User for the first number.
+    var userInput = prompt("Please enter a number.");
+    //Checking to see if the User inputted nothing.
     var check = (userInput == "") ? false : true ;
+    //Conditional to make sure the user didn't input nothing.
     if(check) {
+        //Conditional to check to make sure the user inputted a number.
         if (Number(userInput) || userInput == 0) {
+            //Setting the first number in the array to the inputted number.
             numbers[0] = userInput;
+            //Breaking from the loop.
             break;
         }
     }
 }
 
-//console.log(numbers[0]);
+//Loop for validation.
 while(true)
 {
 //Asking to add, subtract, multiply or divide.
     userInput = prompt("Would you like to Add, Subtract, Multiply or Divide?");
+    //Checking to see if the user inputted nothing.
     var check = (userInput == "") ? false : true ;
+    //Conditional to make sure the user didn't input nothing.
     if(check) {
+        //Setting the string in the array to the inputted string.
         numbers[1] = userInput;
     }
 //console.log(numbers[1]);
 
-//If User puts something other then addition, subtraction, multiplication, or division.
-//Checking to see what the User put in.
+//If User puts something other then addition, subtraction, multiplication, or division, prompt will pop back up.
 
+//Checking to see what the User put in.
     if (numbers[1] == "Add" || numbers[1] == "add" || numbers[1] == "+") {
 
-        //Inputting another number for addition.
+        //Loop for validation.
         while(true) {
+            //Prompting the User to input another number.
             userInput = prompt("Please enter a number to Add.");
+            //Checking to see if the user inputted nothing.
             var check = (userInput == "") ? false : true ;
+            //Conditional to make sure the user didn't input nothing.
             if(check) {
+                //Conditional to check to make sure the user inputted a number.
                 if (Number(userInput) || userInput == 0) {
+                    //Setting the second number in the array to inputted number.
                     numbers[2] = userInput;
+                    //Breaking from the loop.
                     break;
                 }
             }
@@ -58,6 +73,7 @@ while(true)
 
         //Showing the answer to the User.
         console.log(numbers[0] + " + " + numbers[2] + " = " + answer);
+        //Breaking from the loop.
         break;
     }
     else if (numbers[1] == "Subtract" || numbers[1] == "subtract" || numbers[1] == "-") {
