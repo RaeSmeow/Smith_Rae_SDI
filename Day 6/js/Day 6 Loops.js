579626eb5be841875b44e2e5435c9cc58b395249
Rae Smith <rsmith1@fullsail.edu>
2015-03-14 17:26:56 -0400

    More info
    More loops and stuffs.

--- Day 6/js/Day 6 Loops.js
+++ Day 6/js/Day 6 Loops.js
@@ -73,4 +73,28 @@ console.log(I);
 
  //Again I was supposed make a new page but nahh.
  //Validation.
+
+ //Ask the User for a number.
+ var num1 = prompt("Please enter a number.");
+ //Validate that the User typed in a number and didn't leave it blank.
+ //Validate using a while loop.
+ while(isNaN(num1)|| num1 ===""){
+     //Reprompt the User.
+     if(num1 ===""){
+         //The User left it blank.
+         num1 = prompt("Please do not leave blank.\nPlease type in a number.");
+     } else if(isNaN(num1)){
+         num1 = prompt("Please only type in numbers.\nPlease enter a number.");
+     }
+ }
+
+//Ask the User a Yes or No question/
+ var userInput = prompt("Please enter yes or no.");
+ //Convert to lower case.
+ userInput = userInput.toLowerCase();
+ //Validate.
+ while(userInput !="yes" && userInput !="no"){
+//Reprompt.
+     userInput = prompt("Please only enter in yes or no.\nPlease type in ye sor no. ");
+ }
  
