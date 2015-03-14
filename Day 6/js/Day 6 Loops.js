33e7d6e10e86beb6a2709c2d2a4bcac448522873
Rae Smith <rsmith1@fullsail.edu>
2015-03-14 16:34:19 -0400

    Loops
    Loops loops loops and numbers!

--- Day 6/js/Day 6 Loops.js
+++ Day 6/js/Day 6 Loops.js
@@ -9,3 +9,31 @@ Day 6 Loops
 
 //alert("Just testing.");
 
+//Basic While Loop.
+//Start a Counter.
+var counter = 0;
+//Start the while loop.
+//Condition to test goes inside ().
+ while(counter < 200){
+    console.log("Somethings never end.");
+     console.log(counter);
+     //Make a change to the counting variable.
+     counter++;
+ }
+
+//Do While Loop.
+//This loop will run the code first then check a condition.
+/*
+Do{
+    //Code to run
+} while(condition to test);
+ */
+ //Declare a counting variable.
+ var I = 0;
+ do{
+    console.log("The number is " + I);
+ //Change the counting variable.
+     I++
+ } while(I < 100);
+
+ 
