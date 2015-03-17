8e31d323232feb19a5ebe9a74648443af004e39d
Rae Smith <rsmith1@fullsail.edu>
2015-03-17 17:19:37 -0400

    Lab
    Lab stuff.

---
+++ Day 7/js/Lab Functions.js
@@ -0,0 +1,27 @@
+ /*
+Rae Smith
+3-17-15
+Section 00
+Day 7 Lab
+ */
+
+
+//alert("Just testing.");
+
+//Circumference.
+ var radius = 2;
+ var weight = 150;
+ function circumference(radius)
+ {
+    var circumference = 2* Math.PI*radius;
+     return circumference;
+ }
+ console.log(circumference(radius));
+
+ //Stung
+ function beeStings(weight)
+ {
+     var stings = 8.666666667*weight;
+     return stings;
+ }
+ console.log((weight));
