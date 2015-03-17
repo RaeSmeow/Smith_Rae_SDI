a480302d5467d82c00a7f01f108d763f19358ae5
Rae Smith <rsmith1@fullsail.edu>
2015-03-17 14:09:15 -0400

    Zombie Attack!
    For Loop for zombie number problem.

--- Day 7/js/Day 7 Functions.js
+++ Day 7/js/Day 7 Functions.js
@@ -3,13 +3,13 @@
 Rae Smith
 3-17-15
 Section 00
-Day 7 Functions
+Day 7
  */
 
 
 //alert("Just testing.");
 
-//Code the song 99 Bottles of Beer on the Wall.
+//Code the song 99 Bottles of Beer on the Wall. Activity 1.
 //Basic structure of For Loop.
 //for(var I = 0; condition to test; increment of change){}
 for(var I = 99; I > 0; I --) {
@@ -18,4 +18,27 @@ for(var I = 99; I > 0; I --) {
     } else{
         console.log(I + " bottles of beer on the wall. "+I+"  bottles of beer. Take one down, pass it around. "+(I-1)+" bottles of beer on the wall.");
     }
-} 
+}
+
+//Zombies. Activity 2.
+//Zombie Attack!
+//We have accidentally created a zombie at Full Sail.
+//A zombie can bite 4 people a day and infect them the next day.
+//The CDC wants to know how many zombies there will be in 8 days.
+//Givens that we know.
+//How many zombies do we have?
+var numZombies = 1;
+//How many bites per zombie , per day?
+var numBites = 4;
+//Number of days the CDC wants.
+var days = 8;
+//Create a For Loop to calculate the zombie number.
+for(var I = 1; I <= days; I++){
+    //How many new zombies turn every day?
+    var newZombies = numZombies * numBites;
+    //Add the new zombies to the existing horde.
+    numZombies += newZombies;
+    //console.log the results.
+    console.log("There are "+numZombies+" number of zombies on Day "+I+"!");
+}
+//How long will it take to get a million zombies?
