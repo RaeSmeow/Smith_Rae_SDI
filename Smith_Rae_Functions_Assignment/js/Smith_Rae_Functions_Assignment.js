84eb361a1e4016cae4b88b5ad352d326f19e271f
Rae Smith <rsmith1@fullsail.edu>
2015-03-23 13:47:26 -0400

    Assignment
    Assignment mostly done. Need to add comments and a couple ectra things.

--- Smith_Rae_Functions_Assignment/js/Smith_Rae_Functions_Assignment.js
+++ Smith_Rae_Functions_Assignment/js/Smith_Rae_Functions_Assignment.js
@@ -6,3 +6,47 @@ Functions Assignment
 */
 
 //alert("Just testing.");
+//Initializing an array.
+
+function InputNumber()
+{
+    while(true) {
+        var userInput = prompt("Please enter a number.")
+        if(userInput == ""){
+            continue;
+        }
+        if(Number(userInput) || userInput == 0) {
+            return Number(userInput);
+        }
+    }
+}
+function InputSign()
+{
+    while(true) {
+        userInput = prompt("Would you like to Add or Subtract?");
+        if(userInput == "" ){
+            continue;
+        }
+        if (userInput == "add" || numbers[1] == "Add") {
+            return '+';
+        }
+        if (userInput == "Subtract" || numbers[1] == "subtract") {
+            return '-';
+        }
+    }
+}
+function Calculator(first, sign, second)
+{
+    if(sign == '+')
+    {
+        return (first + second) ;
+    }
+    else
+    {
+        return (first - second);
+    }
+}
+
+var answer = Calculator(InputNumber(), InputSign(), InputNumber());
+console.log(answer);
+
