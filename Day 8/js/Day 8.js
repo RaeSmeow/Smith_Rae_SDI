7c1777d31f4d7dc7b88a6394c2747690d582b385
Rae Smith <rsmith1@fullsail.edu>
2015-03-24 14:23:38 -0400

    Done.
    Done with notes for the day. Just need to finish the assignment.

--- Day 8/js/Day 8.js
+++ Day 8/js/Day 8.js
@@ -8,3 +8,16 @@ Day 8
 //alert("Just testing.");
 
 //Random.
+//Create a function that will give us a random integer between two values.
+//Create variables for minimum and maximum variables.
+var min = 22;
+var max = 63;
+//Function call.
+var ranNum = getRandom(min, max);
+console.log("Your random number between "+min+" and "+max+" is "+ranNum+".");
+//Create our function.
+function getRandom(min, max){
+    //Generate a random integer.
+    var randomNumber = Math.round(Math.random()*(max-min));
+    return randomNumber;
+}
