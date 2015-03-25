a165560556ddaf51135d4159cc56aef599cfbcaa
Rae Smith <rsmith1@fullsail.edu>
2015-03-24 22:34:50 -0400

    Almost Finished
    Added the actual word problem and made changes. All I have to do is add
    comments.

--- Smith_Rae_Functions_Assignment/js/Smith_Rae_Functions_Assignment.js
+++ Smith_Rae_Functions_Assignment/js/Smith_Rae_Functions_Assignment.js
@@ -23,38 +23,76 @@ function InputNumber()
 
 function InputSign() {
     while (true) {
-        userInput = prompt("Would you like to Add, Subtract, Multiply, or Divide?");
+        var userInput = prompt("Would you like to Add,Subtract, Multiply, or Divide?");
         if (userInput == "") {
             continue;
         }
-        if (userInput == "Add" || numbers[1] == "add") {
+        if (userInput == "Add" || userInput == "add") {
             return '+';
         }
-        if (userInput == "Subtract" || numbers[1] == "subtract") {
+        if (userInput == "Subtract" || userInput == "subtract") {
             return '-';
         }
-        if (userInput == "Multiply" || numbers[1] == "multiply") {
+        if (userInput == "Multiply" || userInput == "multiply") {
             return '*';
         }
-        if (userInput == "Divide" || numbers[1] == "divide") {
+        if (userInput == "Divide" || userInput == "divide") {
             return '/';
         }
     }
+}
     function Calculator(first, sign, second) {
         if (sign == '+') {
             return (first + second);
         }
-        else {
+        else if(sign == '-'){
             return (first - second);
         }
+        else if(sign == '*'){
+            return (first * second);
+        }
+        else{
+            return (first/second);
+        }
     }
-    var answer = Calculator(InputNumber(), InputSign(), InputNumber());
-    console.log(answer);
-}
+
+    console.log("You are living in an apartment with 4 people. Your water bill is $128");
+    console.log("Please you the calculator to find the correct amount of money for you to pay.")
+    var first = InputNumber();
+    var sign = InputSign();
+    var second = InputNumber();
+    var answer = Calculator(first, sign, second);
+    console.log(first+" "+sign+" "+second+" = "+answer);
+
+    console.log("You are living in an apartment with 4 people. Your portion of rent is $427.");
+    console.log("Please you the calculator to find the overall cost of rent.")
+    var first = InputNumber();
+    var sign = InputSign();
+    var second = InputNumber();
+    var answer = Calculator(first, sign, second);
+    console.log(first+" "+sign+" "+second+" = "+answer);
+
+    console.log("You are paying $126 for an animal each month. another person pays $246 for the same animal.");
+    console.log("Please you the calculator to find the correct amount of money you spend overall on the animal.")
+    var first = InputNumber();
+    var sign = InputSign();
+    var second = InputNumber();
+    var answer = Calculator(first, sign, second);
+    console.log(first+" "+sign+" "+second+" = "+answer);
+
+    console.log("You are paying $327 on something that costs $726");
+    console.log("Please you the calculator to find the correct amount of money that someone else has to pay.")
+    var first = InputNumber();
+    var sign = InputSign();
+    var second = InputNumber();
+    var answer = Calculator(first, sign, second);
+    console.log(first+" "+sign+" "+second+" = "+answer);
+
+
 
 //Test values.
-//Addition: 7+8=15.
-//Subtraction: 25-17=8.
-//Multiplication: 7*6=42.
-//Division: 8/2=4.
-//The End. 
+//Addition: 126+246=372.
+//Subtraction: 726-327=399.
+//Multiplication: 4*427=1736.
+//Division: 128/4=32.
+//The End.
